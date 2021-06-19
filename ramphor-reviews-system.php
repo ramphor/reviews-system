aa1db518e4b2f6322e7d7df6fe1ab3bdc98c376e
Puleeno Nguyen <puleeno@gmail.com>
2021-06-20 03:02:53 +0700

    Create plugin metadata

---
+++ ramphor-reviews-system.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Ramphor Review System
+ * Author: Ramphor Premium
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: The reviews system WordPress plugin by Ramphor Premium
+ */
